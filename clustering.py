0d9fa31e112fefc12f3b4fd21daa51e074d5377e
Ali Cheraghian <alichr64@gmail.com>
2023-07-19 12:42:31 +1000

    Done

--- clustering.py
+++ clustering.py
@@ -1,5 +1,5 @@
 import numpy as np
 import torch
-from foundation import clip
-import torch.nn as nn
-from utils.mv_utils_zs import Realistic_Projection
+# from foundation import clip
+# import torch.nn as nn
+# from utils.mv_utils_zs import Realistic_Projection
