149e58a900b5f97cf19164305d26d000e9d8042e
Ali Cheraghian <alichr64@gmail.com>
2023-07-19 14:09:48 +1000

    clip

--- clustering.py
+++ clustering.py
@@ -1,5 +1,24 @@
 import numpy as np
+import sklearn
 import torch
-# from foundation import clip
-# import torch.nn as nn
-# from utils.mv_utils_zs import Realistic_Projection
+from foundation import clip
+import torch.nn as nn
+from utils.mv_utils_zs import Realistic_Projection
+
+
+
+## define dataset and dataloader
+
+
+
+## define model
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+
+
+print(model.encode_image())
+
+    
+
+
