5dab3e22df3b9f29038c0f7acb7609d38b9afb3d
Ali Cheraghian <alichr64@gmail.com>
2023-07-23 23:43:46 +1000

    Done

--- clustering.py
+++ clustering.py
@@ -1,19 +1,23 @@
 import numpy as np
-import sklearn
+#import sklearn
 import torch
 from foundation import clip
 import torch.nn as nn
 from utils.mv_utils_zs import Realistic_Projection
-from transformers import CLIPProcessor, CLIPModel
+#from transformers import CLIPProcessor, CLIPModel
+import open_clip
+
 
 ## define dataset and dataloader
 
 
 
 ## define model
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+#model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 # device = "cuda" if torch.cuda.is_available() else "cpu"
 # model, preprocess = clip.load("ViT-B/32", device=device)
+print(model)
 
 
 
