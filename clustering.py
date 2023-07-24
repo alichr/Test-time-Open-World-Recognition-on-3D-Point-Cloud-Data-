ab169eb5130ee7c4c631e45c7f50485414e2df7a
Ali Cheraghian <alichr64@gmail.com>
2023-07-24 17:30:55 +1000

    Done

--- clustering.py
+++ clustering.py
@@ -1,38 +1,42 @@
 import numpy as np
 import torch
-from foundation import clip
 import torch.nn as nn
-from utils.mv_utils_zs import Realistic_Projection
-#from transformers import CLIPProcessor, CLIPModel
-import open_clip
 from PIL import Image
+from sklearn.cluster import KMeans
 
-## define dataset and dataloader
+# Import any other required modules for the dataset and dataloader if needed
+# (These parts are currently commented out)
 
+import open_clip
+from utils.mv_utils_zs import Realistic_Projection
 
-## define clip model
+## Define CLIP model
 def clip_model():
+    # Create a CLIP model with a specified architecture and pre-trained weights
     model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
     return model, preprocess
 
-## define projection
+## Define projection
 def projection():
+    # Create a Realistic Projection object
     proj = Realistic_Projection()
     return proj
 
-## define clustering
+## Define clustering
 def clustering(X, n_clusters=10):
-    from sklearn.cluster import KMeans
+    # Use KMeans algorithm to cluster data X into n_clusters groups
     kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
     return kmeans
 
-# define SVD for creating subspace using pytroch
+# Define SVD for creating subspace using PyTorch
 def SVD(X, n_components=10):
+    # Perform Singular Value Decomposition (SVD) on data X using PyTorch
     U, S, V = torch.svd(X)
     return U[:, :n_components]
 
 
 print(clip_model())
+
     
 
 
