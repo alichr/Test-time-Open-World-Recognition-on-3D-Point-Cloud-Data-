892ce65b09558a0137be8fd71c78c373b61df85c
Ali Cheraghian <alichr64@gmail.com>
2023-07-25 13:39:57 +1000

    Done

--- clustering.py
+++ clustering.py
@@ -10,81 +10,106 @@ from sklearn.cluster import KMeans
 import open_clip
 from utils.mv_utils_zs import Realistic_Projection
 
+# Check if a CUDA-enabled GPU is available, otherwise use CPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 ## Define CLIP model
 def clip_model():
-    # Create a CLIP model with a specified architecture and pre-trained weights
+    """
+    Create and return a CLIP model with a specified architecture and pre-trained weights.
+
+    Returns:
+    - model: The CLIP model.
+    - preprocess: The preprocessing function to prepare images for the model.
+    """
     model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
     return model, preprocess
 
 ## Define projection
 def projection():
-    # Create a Realistic Projection object
+    """
+    Create and return a Realistic Projection object.
+
+    Returns:
+    - proj: Realistic Projection object.
+    """
     proj = Realistic_Projection()
     return proj
 
 ## Define clustering
 def clustering(X, n_clusters=10):
-    # Use KMeans algorithm to cluster data X into n_clusters groups
+    """
+    Cluster data X into n_clusters groups using KMeans algorithm.
+
+    Parameters:
+    - X: The data to be clustered.
+    - n_clusters: The number of clusters to create.
+
+    Returns:
+    - kmeans: KMeans model fitted to the data X.
+    """
     kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
     return kmeans
 
-# Define SVD for creating subspace using PyTorch
+# Define SVD for creating a subspace using PyTorch
 def SVD(X, n_components=10):
-    # Perform Singular Value Decomposition (SVD) on data X using PyTorch
+    """
+    Perform Singular Value Decomposition (SVD) on data X using PyTorch.
+
+    Parameters:
+    - X: The data for SVD.
+    - n_components: The number of top singular values and vectors to keep.
+
+    Returns:
+    - U: Left singular vectors of X.
+    - S: Singular values of X.
+    - V: Right singular vectors of X.
+    """
     U, S, V = torch.svd(X)
     return U[:, :n_components]
 
 
 ## Define main function
 def main():
+    """
+    Main function to execute the clustering process.
 
-    # Load CLIP model
+    Steps:
+    1. Load CLIP model.
+    2. Load Realistic Projection object.
+    3. Generate random point cloud samples.
+    4. Forward samples through the CLIP model to get feature vectors.
+    5. Cluster the feature vectors using KMeans.
+    """
+    # Step 1: Load CLIP model
     model, preprocess = clip_model()
 
-    # Load Realistic Projection object
+    # Step 2: Load Realistic Projection object
     proj = projection()
 
-    # create 100 random samples using torch
+    # Step 3: Create 100 random samples using torch
     pc = torch.rand(100, 1024, 3)
-    # define a feature vecros size (100, 512)
+    # Define a feature vectors size (100, 512)
     feature_vectors = np.zeros((100, 512))
 
-    # forwad samples to clip model
+    # Step 4: Forward samples to the CLIP model
     with torch.no_grad():
         for i in range(100):
-            # project samples to image
+            # Project samples to an image
             pc_prj = proj.get_img(pc[i,:,:].unsqueeze(0))
             pc_img = torch.nn.functional.interpolate(pc_prj, size=(224, 224), mode='bilinear', align_corners=True)  
             pc_img = pc_img.to(device)
-            # forward samples to clip model
+            # Forward samples to the CLIP model
             pc_img = model.encode_image(pc_img)
             pc_img = pc_img.cpu().numpy()
             pc_img_avg = np.mean(pc_img, axis=0)        
-            # save feature vectors
+            # Save feature vectors
             feature_vectors[i,:] = pc_img_avg
             print(i)
-    # cluster samples
+    
+    # Step 5: Cluster samples
     kmeans = clustering(feature_vectors, n_clusters=10)
-
-
-
 
 if __name__ == "__main__":
     main()
     print("Done!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
