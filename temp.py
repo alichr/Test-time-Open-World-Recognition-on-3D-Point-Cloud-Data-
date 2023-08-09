b1fb07dabaed91e61d161c18272c28efbd698870
Ali Cheraghian <alichr64@gmail.com>
2023-08-09 16:18:28 +1000

    modified

--- temp.py
+++ temp.py
@@ -1,16 +1,21 @@
 import numpy as np
+import os
 
 
-# randam array with numpy szie (512,100)
-a = np.random.rand(512,10000)
+# load subspace folder
+def subsapce():
+    """
+    Create and return a subsapce memory.
+    """
+    Subsapce = {}
+    for file in os.listdir("subspace/"): 
+        Subsapce[file[:-4]] = np.load("subspace/"+file)
+    return Subsapce
 
-# apply svd
-U, S, V = np.linalg.svd(a, full_matrices=False)
-print(S)
+Subsapce = subsapce()
 
-energy = np.cumsum(S)/np.sum(S)
-n_components = np.where(energy>0.95)[0][0]
-print(energy)
-print(n_components)
 
-print(1131.62567495 / np.sum(S))
+for i in range(len(Subsapce.keys())):
+    print(Subsapce['subspace'+str(i)].shape)
+
+
